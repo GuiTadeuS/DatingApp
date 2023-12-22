5b09c3fa900c8b4432edf4992fe03078acc4ed11
guilherme <tadeu.guilherme2002@gmail.com>
2023-12-21 23:55:18 -0300

    feat: add user controller get methods

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/users")] // api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUSers()
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet("{id}")] // /api/users/id
+        public ActionResult<AppUser> GetUser(int id) {
+            
+            var user = _context.Users.Find(id);
+            
+            if (user == null)
+            {
+                return NotFound();
+            } 
+            return user;
+        }
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,10 +1,13 @@
-﻿namespace API.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
 {
     public class AppUser
     {
+        [Key]
         public int Id { get; set; }
         public string? UserName { get; set; }
 
-
+        
     }
 }
