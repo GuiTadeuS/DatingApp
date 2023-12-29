29fb6dff9b1c02d4b5cfc6bddc78fa74e421c641
guilherme <tadeu.guilherme2002@gmail.com>
2023-12-29 08:05:20 -0300

    feat: add base api controller and account controller for user registration

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController: BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")] // api/account/register
+        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        {
+            using var hmac = new HMACSHA512(); // password salt
+
+            var user = new AppUser
+            {
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)), // password hash
+                PasswordSalt = hmac.Key                                            // password salt randonly generated
+            };
+
+            _context.Users.Add(user);                                              // add user to context
+            await _context.SaveChangesAsync();                                     // save changes to db
+            return user;
+        }
+    }
+}
