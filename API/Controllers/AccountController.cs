c0e39304bfefb3eb9baf388dddddc9b6d1aa5953
guilherme <tadeu.guilherme2002@gmail.com>
2024-01-22 23:56:58 -0300

    feat: add register dto and user exist method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -15,21 +17,28 @@ namespace API.Controllers
             _context = context;
         }
 
-        [HttpPost("register")] // api/account/register
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        [HttpPost("register")] // api/account/register?
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
             using var hmac = new HMACSHA512(); // password salt
 
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)), // password hash
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)), // password hash
                 PasswordSalt = hmac.Key                                            // password salt randonly generated
             };
 
             _context.Users.Add(user);                                              // add user to context
             await _context.SaveChangesAsync();                                     // save changes to db
             return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());     // check if user exists
         }
     }
 }
--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -2,5 +2,8 @@
 {
     public class RegisterDto
     {
+        public string Username { get; set; }
+
+        public string Password { get; set; } 
     }
 }
