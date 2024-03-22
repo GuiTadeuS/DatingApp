371cefb991d3f4e8392a1562930c5601496d759d
guilherme <tadeu.guilherme2002@gmail.com>
2024-03-22 00:56:56 -0300

    fix: age type

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -43,6 +43,12 @@ namespace API.Data
 
             query = query.Where(u => u.DateOfBirth >= minDate && u.DateOfBirth <= maxDate);
 
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(u => u.Created),
+                _ => query.OrderByDescending(u => u.LastActive)
+            };
+
             return await PagedList<MemberDto>.CreateAsync(
                 query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider), 
                 userParams.PageNumber, userParams.PageSize);
